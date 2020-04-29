2d7dbaf78dd7f408ab4ee4c5aeff7ab8b508986b
Sol Zilberman <solzilbermancode@gmail.com>
2020-04-29 00:50:48 -0500

    Updates

---
+++ precache-manifest.3a6baa0a2ccccb8a4529e04673aae01f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc2d52973c283591968d2761954bf92",
+    "url": "/learnToCode/index.html"
+  },
+  {
+    "revision": "9b35c2e51743c4918ede",
+    "url": "/learnToCode/static/css/main.f9aff6ce.chunk.css"
+  },
+  {
+    "revision": "20733c5f260cf443acc4",
+    "url": "/learnToCode/static/js/2.38e86a2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/learnToCode/static/js/2.38e86a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b35c2e51743c4918ede",
+    "url": "/learnToCode/static/js/main.24d5a0a9.chunk.js"
+  },
+  {
+    "revision": "7bba42e2ce1cf5e88a65",
+    "url": "/learnToCode/static/js/runtime-main.e261cdd1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learnToCode/static/media/logo.5d5d9eef.svg"
+  }
+]);
